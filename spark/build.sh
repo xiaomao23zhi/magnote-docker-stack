beb87752d9ef6aa71ce346678c984052e7864683
xiaomao23zhi <gokasama@icloud.com>
2022-08-09 11:54:46 +0800

    Create build.sh

---
+++ spark/build.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+set -ex
+
+SPARK_VERSION=3.3.0
+HADOOP_VERSION=hadoop3
+SPARK_BIN=spark-${SPARK_VERSION}-bin-${HADOOP_VERSION}.tgz
+AWS_JAVA_SDK_BUNDLE_VERSION=1.11.1034
+HADOOP_AWS_VERSION=3.2.4
+REPO=gchr.io/xiaomao23zhi/magnote-docker-stack
+TAG=${SPARK_VERSION}_${HADOOP_VERSION}
+
+wget SPARK_BIN_URL=https://archive.apache.org/dist/spark/spark-${SPARK_VERSION}/spark-${SPARK_VERSION}-bin-${HADOOP_VERSION}.tgz
+tar -xzf ${SPARK_BIN}
+
+# Install addtional jars
+cd spark-${SPARK_VERSION}-bin-${HADOOP_VERSION}/jars
+wget https://repo1.maven.org/maven2/com/amazonaws/aws-java-sdk-bundle/${AWS_JAVA_SDK_BUNDLE_VERSION}/aws-java-sdk-bundle-${AWS_JAVA_SDK_BUNDLE_VERSION}.jar
+wget https://repo1.maven.org/maven2/org/apache/hadoop/hadoop-aws/${HADOOP_AWS_VERSION}/hadoop-aws-${HADOOP_AWS_VERSION}.jar 
+
+cd -
+./bin/docker-image-tool.sh -r ${REPO} -t ${TAG} -p ./kubernetes/dockerfiles/spark/bindings/python/Dockerfile build
