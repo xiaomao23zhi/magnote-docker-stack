0ff041acbdeb2343dfe03a0e0588e0381170ac95
wujia <wujia@chinamobile.com>
2022-08-09 12:13:42 +0800

    fix

--- spark/build.sh
+++ spark/build.sh
@@ -10,13 +10,13 @@ HADOOP_AWS_VERSION=3.2.4
 REPO=gchr.io/xiaomao23zhi/magnote-docker-stack
 TAG=${SPARK_VERSION}_${HADOOP_VERSION}
 
-wget https://archive.apache.org/dist/spark/spark-${SPARK_VERSION}/spark-${SPARK_VERSION}-bin-${HADOOP_VERSION}.tgz
-tar spark-${SPARK_VERSION}-bin-${HADOOP_VERSION}.tgz
+wget -q https://archive.apache.org/dist/spark/spark-${SPARK_VERSION}/spark-${SPARK_VERSION}-bin-${HADOOP_VERSION}.tgz
+tar -xvf spark-${SPARK_VERSION}-bin-${HADOOP_VERSION}.tgz
 
 # Install addtional jars
 cd spark-${SPARK_VERSION}-bin-${HADOOP_VERSION}
-wget https://repo1.maven.org/maven2/com/amazonaws/aws-java-sdk-bundle/${AWS_JAVA_SDK_BUNDLE_VERSION}/aws-java-sdk-bundle-${AWS_JAVA_SDK_BUNDLE_VERSION}.jar jars/
-wget https://repo1.maven.org/maven2/org/apache/hadoop/hadoop-aws/${HADOOP_AWS_VERSION}/hadoop-aws-${HADOOP_AWS_VERSION}.jar jars/
+wget -q https://repo1.maven.org/maven2/com/amazonaws/aws-java-sdk-bundle/${AWS_JAVA_SDK_BUNDLE_VERSION}/aws-java-sdk-bundle-${AWS_JAVA_SDK_BUNDLE_VERSION}.jar jars/
+wget -q https://repo1.maven.org/maven2/org/apache/hadoop/hadoop-aws/${HADOOP_AWS_VERSION}/hadoop-aws-${HADOOP_AWS_VERSION}.jar jars/
 
 # Build images
 ./bin/docker-image-tool.sh -r ${REPO} -t ${TAG} -p ./kubernetes/dockerfiles/spark/bindings/python/Dockerfile build
