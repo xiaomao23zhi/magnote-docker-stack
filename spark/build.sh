0a24f68eb929b5629629c3a192cc24476ed7a069
wujia <wujia@chinamobile.com>
2022-08-09 12:36:51 +0800

    fix

--- spark/build.sh
+++ spark/build.sh
@@ -7,7 +7,7 @@ HADOOP_VERSION=hadoop3
 AWS_JAVA_SDK_BUNDLE_VERSION=1.11.1034
 HADOOP_AWS_VERSION=3.2.4
 
-REPO=gchr.io/xiaomao23zhi/magnote-docker-stack
+REPO=ghcr.io/xiaomao23zhi/magnote-docker-stack
 TAG=${SPARK_VERSION}_${HADOOP_VERSION}
 
 wget -q https://archive.apache.org/dist/spark/spark-${SPARK_VERSION}/spark-${SPARK_VERSION}-bin-${HADOOP_VERSION}.tgz
